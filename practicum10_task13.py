b67b5b095d4941356c6f1355080c265d0adbb8d5
ivanova-12 <ivanov.sim-1977@yandex.ru>
2026-03-27 21:13:31 +0700

    Add files via upload

---
+++ practicum10_task13.py
@@ -0,0 +1,16 @@
+import turtle as t
+
+def triangle(v1: tuple, v2: tuple, v3: tuple, color: str) -> None:
+    """Draw a triangle with help 3 vertex and color"""
+    t.up()
+    t.pencolor(color)
+    t.goto(v1)
+    t.down()
+    t.fillcolor(color)
+    t.begin_fill()
+    t.goto(v2)
+    t.goto(v3)
+    t.goto(v1)
+    t.end_fill()
+
+
